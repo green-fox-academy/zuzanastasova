30c453d373aff7ff2eed201eca16d098462ea15a
Zuzana <saska.stasova@gmail.com>
2020-09-03 13:49:44 +0200

    add TwoNumbers

---
+++ week-01/day4/src/com/company/TwoNumbers.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class TwoNumbers {
+    public static void main(String[] args) {
+        // Create a program that prints a few operations on two numbers: 22 and 13
+        int a = 22;
+        int b = 13;
+        // Print the result of 13 added to 22
+        System.out.println(a+b);
+        // Print the result of 13 substracted from 22
+        System.out.println(a-b);
+        // Print the result of 22 multiplied by 13
+        System.out.println(a*b);
+        // Print the result of 22 divided by 13 (as a decimal fraction)
+        double b2 = 13;
+        double c = a/b2;
+        System.out.println(c);
+        // Print the integer part of 22 divided by 13
+        System.out.println(a/b);
+        // Print the remainder of 22 divided by 13
+        System.out.println(a%b);
+    }
+}
