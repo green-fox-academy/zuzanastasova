2d9e1ec4fda93e6bb01f00f9b1998f83b4c38fb0
Zuzana <saska.stasova@gmail.com>
2020-09-03 14:31:55 +0200

    add SwapNumbers

---
+++ week-01/day4/src/com/company/Swap.java
@@ -0,0 +1,16 @@
+package com.company;
+
+
+public class Swap {
+    public static void main(String[] args) {
+        // Swap the values of the variables
+        int a = 123;
+        int b = 526;
+        int number = a;
+        a = b;
+        b = number;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
