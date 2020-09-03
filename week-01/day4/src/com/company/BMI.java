0af8ca456f8fdf619c5ccb064897fc2ff8154390
Zuzana <saska.stasova@gmail.com>
2020-09-03 14:40:22 +0200

    add BMI

---
+++ week-01/day4/src/com/company/BMI.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class BMI {
+    public static void main(String[] args) {
+        double massInKg = 81.2;
+        double heightInM = 1.78;
+        double calBMI = massInKg/(heightInM*heightInM);
+        System.out.println(calBMI);
+        // Print the Body mass index (BMI) based on these values
+    }
+}
