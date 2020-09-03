3af00a261e0a8e0533a24f2e81606daf852507b5
Zuzana <saska.stasova@gmail.com>
2020-09-03 20:49:39 +0200

    add OneTwoALot

---
+++ week-01/day4/src/com/company/src/OneTwoALot.java
@@ -0,0 +1,24 @@
+public class OneTwoALot {
+    public static void main(String[] args) {
+
+        int i=2;
+        if (i<=0) {
+            System.out.println("Not enough");
+        }
+        else if (i == 1) {
+            System.out.println("One");
+        }
+        else if (i == 2) {
+            System.out.println("Two");
+        }
+        else {
+            System.out.println("A lot");
+        }
+
+// Write a program that reads a number form the standard input,
+// If the number is zero or smaller it should print: Not enough
+// If the number is one it should print: One
+// If the number is two it should print: Two
+// If the number is more than two it should print: A lot
+    }
+}
