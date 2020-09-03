a80c0686ec582f2c4f82b14339db8d988563f7d8
Zuzana <saska.stasova@gmail.com>
2020-09-03 13:42:49 +0200

    add IntroduceYourself

---
+++ week-01/day4/src/com/company/IntroduceYourself.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        // Write a program that prints a few details to the terminal window about you
+        // It should print each detail to a new line.
+        //  - Your name
+        //  - Your age
+        //  - Your height in meters (as a decimal fraction)
+        System.out.println("Zuzana Stašová");
+        int myage = 26;
+        System.out.println(myage);
+        double myheight = 1.64;
+        System.out.println(myheight);
+    }
+}
