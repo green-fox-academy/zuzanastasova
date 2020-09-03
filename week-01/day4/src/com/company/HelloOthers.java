9e83b5a93cc7bb3d679e60beb7c12142b2882f28
Zuzana <saska.stasova@gmail.com>
2020-09-03 13:37:32 +0200

    add Hello Others

---
+++ week-01/day4/src/com/company/HelloOthers.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello, Esther!");
+        System.out.println("Hello, Mary!");
+        System.out.println("Hello, Joe!");
+    }
+}
