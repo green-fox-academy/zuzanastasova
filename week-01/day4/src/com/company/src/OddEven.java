2a095494599c8163d9bde4d74a49a622fcebd010
Zuzana <saska.stasova@gmail.com>
2020-09-03 20:43:09 +0200

    add OddorEven

---
+++ week-01/day4/src/com/company/src/OddEven.java
@@ -0,0 +1,11 @@
+public class OddEven {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = i % 2;
+        if (j == 0) {
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");
+        }
+    }
+}
