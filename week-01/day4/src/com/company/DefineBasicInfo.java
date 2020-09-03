8a2b6d24e2592800cab8df10803655b1cec4d869
Zuzana <saska.stasova@gmail.com>
2020-09-03 14:48:15 +0200

    add PersonalInfo

---
+++ week-01/day4/src/com/company/DefineBasicInfo.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        String myName = "Zuzana";
+        // Your age as an integer
+        int myAge = 26;
+        // Your height in meters as a double
+        double myHeight = 1.64;
+        // Whether you are married or not as a boolean
+        boolean maritalStatus = false;
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(myHeight);
+        System.out.println(maritalStatus);
+    }
+}
