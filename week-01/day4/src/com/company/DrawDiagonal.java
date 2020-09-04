3a5891ac75eac6d53eb1daf81df6b014e4d89cb1
Zuzana <saska.stasova@gmail.com>
2020-09-05 01:24:48 +0200

    add DrawDiagonal

--- week-01/day4/src/com/company/DrawDiagonal.java
+++ week-01/day4/src/com/company/DrawDiagonal.java
@@ -8,12 +8,11 @@ public class DrawDiagonal {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Insert whole number, please:");
         int number = scanner.nextInt();
-        System.out.print("*".repeat(number) + "\n");
+        System.out.print("%".repeat(number) + "\n");
         for (int rows = 0; rows < number-2; rows++) {
-            System.out.println("*" + " ".repeat(rows) + "*" + " ".repeat(number-rows-3) + "*" );
-
+            System.out.println("%" + " ".repeat(rows) + "%" + " ".repeat(number-rows-3) + "%" );
             }
-        System.out.print("*".repeat(number));
+        System.out.print("%".repeat(number));
         // Write a program that reads a number from the standard input, then draws a
 // square like this:
 //
