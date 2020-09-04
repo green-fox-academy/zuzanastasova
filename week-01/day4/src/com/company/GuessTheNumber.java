6a6f1fa33d715fd9e5dd09a9c06cda7e0f0e54d0
Zuzana <saska.stasova@gmail.com>
2020-09-04 16:50:22 +0200

    add GuesTheNumber

---
+++ week-01/day4/src/com/company/GuessTheNumber.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        Random generator = new Random ();
+        int randomNumber = generator.nextInt(100 );
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("I am thinking of a whole number between 0 and 100, what do you think it is?");
+        int number = userInput.nextInt();
+        int count = 0;
+        while (number != randomNumber) {
+            if (randomNumber>number) {
+                System.out.println("Higher!");
+            }
+            else {
+                System.out.println("Lower!");
+            }
+            number = userInput.nextInt();
+        }
+        System.out.println("You fount the number! It is " + randomNumber + ".");
+
+
+// Write a program that stores a number, and the user has to figure it out.
+// The user can input guesses, after each guess the program would tell one
+// of the following:
+//
+// The stored number is higher
+// The stried number is lower
+// You found the number: 8
+    }
+}
