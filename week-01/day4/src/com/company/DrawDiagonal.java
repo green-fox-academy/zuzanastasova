d47e8b9c6e50ef0c47bbcaffc329814f9598c577
Zuzana <saska.stasova@gmail.com>
2020-09-05 01:10:55 +0200

    add DrawDiagonal

---
+++ week-01/day4/src/com/company/DrawDiagonal.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class DrawDiagonal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insert whole number, please:");
+        int number = scanner.nextInt();
+        System.out.print("*".repeat(number) + "\n");
+        for (int rows = 0; rows < number-2; rows++) {
+            System.out.println("*" + " ".repeat(rows) + "*" + " ".repeat(number-rows-3) + "*" );
+
+            }
+        System.out.print("*".repeat(number));
+        // Write a program that reads a number from the standard input, then draws a
+// square like this:
+//
+//
+// %%%%%
+// %%  %
+// % % %
+// %  %%
+// %%%%%
+//
+// The square should have as many lines as the number was
+    }
+}
