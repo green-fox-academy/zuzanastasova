86bbc3179d753b28cf3ec5f64ccbe87f9c3787a8
Zuzana <saska.stasova@gmail.com>
2020-09-07 16:23:21 +0200

    add Chinchilla

---
+++ week-02/day1/src/com/company/AppendAFunc.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class AppendAFunc {
+    public static void main(String[] args) {
+       String typo = "Chinchill";
+        appendAFunc(typo);
+    }
+ public static void appendAFunc (String Name) {
+     System.out.println(Name + "a");
+ }
+
+}
