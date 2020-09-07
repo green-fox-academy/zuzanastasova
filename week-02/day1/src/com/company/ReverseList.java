340312ed44b985d687b497a0c25989d8131f45d4
Zuzana <saska.stasova@gmail.com>
2020-09-07 15:36:38 +0200

    add ReverseList

---
+++ week-02/day1/src/com/company/ReverseList.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class ReverseList {
+    public static void main(String[] args) {
+        int [] numbers = {3, 4, 5, 6, 7};
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            System.out.println(numbers[i]);
+        }
+
+
+// - Reverse the order of the elements in `numbers`
+// - Print the elements of the reversed `numbers`
+    }
+}
+
