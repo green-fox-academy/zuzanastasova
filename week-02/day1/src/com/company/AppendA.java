dbdfd15a377dc905c2655a88c35b6fcff36e3889
Zuzana <saska.stasova@gmail.com>
2020-09-07 14:49:27 +0200

    add AppendA

---
+++ week-02/day1/src/com/company/AppendA.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class AppendA {
+    public static void main(String[] args) {
+    String [] animals = {"koal", "pand", "zebr"};
+        for (int i = 0; i < animals.length; i++) {
+            animals [i] = animals [i] + "a";
+            System.out.println(animals[i]);
+        }
+// - Add all elements an `"a"` at the end
+    }
+}
