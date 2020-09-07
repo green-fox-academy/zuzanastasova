61416e377024b04c773134c550338d11d662ab5a
Zuzana <saska.stasova@gmail.com>
2020-09-07 15:54:55 +0200

    Doubling

--- week-02/day1/src/com/company/AppendA.java
+++ week-02/day1/src/com/company/AppendA.java
@@ -4,7 +4,7 @@ public class AppendA {
     public static void main(String[] args) {
     String [] animals = {"koal", "pand", "zebr"};
         for (int i = 0; i < animals.length; i++) {
-            animals [i] = animals [i] + "a";
+            animals [i] += "a";
             System.out.println(animals[i]);
         }
 // - Add all elements an `"a"` at the end
