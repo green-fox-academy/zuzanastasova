2f343a789f1fb9f6a21bcd66588c55d1e47eeede
Zuzana <saska.stasova@gmail.com>
2020-09-07 15:21:09 +0200

    add SumAll

--- week-02/day1/src/com/company/SumAll.java
+++ week-02/day1/src/com/company/SumAll.java
@@ -4,7 +4,11 @@ public class SumAll {
     public static void main(String[] args) {
 
         int [] numbers = {3, 4, 5, 6, 7};
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers [i];
 
+        } System.out.println(sum);
 // - Print the sum of the elements in `numbers`
     }
 }
