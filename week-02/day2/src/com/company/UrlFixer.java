85d8d9e7e005e310c7d0a6c631a033bc1478470c
Zuzana <saska.stasova@gmail.com>
2020-09-08 14:14:47 +0200

    add UrlFixer

---
+++ week-02/day2/src/com/company/UrlFixer.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class UrlFixer {
+        public static void main(String... args) {
+            String url = "https//www.reddit.com/r/nevertellmethebots";
+            url = url.replace ("bots", "odds");
+            url = url.replace ("https", "https:");
+
+            // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+            // Also, the URL is missing a crucial component, find out what it is and insert it too!
+            // Try to solve it more than once using different String functions!
+
+            System.out.println(url);
+        }
+}
+
