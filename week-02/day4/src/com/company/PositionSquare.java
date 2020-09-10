7a83ab7e72b4819fbaa6d0f1bcf66dd8bf7ada6f
Zuzana <saska.stasova@gmail.com>
2020-09-10 18:11:36 +0200

    add PositionSquare

--- week-02/day4/src/com/company/PositionSquare.java
+++ week-02/day4/src/com/company/PositionSquare.java
@@ -6,24 +6,26 @@ import java.util.Scanner;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class GoToCenter {
-    public static void mainDraw(Graphics graphics) {
-        // Create a function that draws a single line and takes 3 parameters:
-        // The x and y coordinates of the line's starting point and the graphics
-        // and draws a line from that point to the center of the canvas.
-        // Draw at least 3 lines with that function using a loop.
+public class PositionSquare {
+
+    public static void mainDraw(Graphics graphics){
+        // create a function that draws one square and takes 3 parameters:
+        // the x and y coordinates of the square's top left corner and the graphics
+        // and draws a 50x50 square from that point.
+        // draw 3 squares with that function.
+        // avoid code duplication.
         Scanner scanner = new Scanner(System.in);
         System.out.println("Write a whole number, please:");
         int startX = scanner.nextInt();
         System.out.println("Write another whole number, please:");
         int startY = scanner.nextInt();
-        for (int i = 0; i < 3; i ++) {
-            drawing(startX, startY, graphics);
-            startX += 20;
-        }
+            drawSquare(graphics, startX, startY);
+
     }
-    public static void drawing (int x, int y, Graphics graphics) {
-        graphics.drawLine(x, y, WIDTH/2, HEIGHT/2);
+    public static void drawSquare (Graphics graphics, int x, int y){
+        for (int i = 10; i < 16; i+=2) {
+            graphics.drawRect(x * i, y * i, 50, 50);
+        }
     }
 
     // Don't touch the code below
@@ -48,4 +50,5 @@ public class GoToCenter {
             mainDraw(graphics);
         }
     }
+
 }
