5130ea83b622cfc57c764a34d0d830c965280915
Zuzana <saska.stasova@gmail.com>
2020-09-10 13:37:16 +0200

    add Drawing

--- week-02/day4/src/com/company/Diagonals.java
+++ week-02/day4/src/com/company/Diagonals.java
@@ -4,19 +4,23 @@ import javax.swing.*;
 import java.awt.*;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class ColoredBox {
+public class Diagonals {
     public static void mainDraw(Graphics graphics) {
-        // Draw a box that has different colored lines on each edge.
-        graphics.setColor (Color.GREEN);
-        graphics.drawLine(10, 10, 50, 10);
-        graphics.setColor(Color.RED);
-        graphics.drawLine(10, 10, 10, 50);
-        graphics.setColor(Color.BLUE);
-        graphics.drawLine(10, 50, 50,50);
-        graphics.setColor(Color.BLACK);
-        graphics.drawLine(50, 10, 50, 50);
-
-
+        // Draw the canvas' diagonals.
+        // If it starts from the upper-left corner it should be green, otherwise it should be red.
+        for (int i = 0; i <= WIDTH; i+=WIDTH/5) {
+            int x = 0;
+            int y = 0;
+            if (i == 0){
+                graphics.setColor(Color.GREEN);
+            } else if (i == WIDTH){
+                graphics.setColor(Color.RED);
+            }
+            else {
+                graphics.setColor(Color.BLACK);
+            }
+            graphics.drawLine(x + i, y, WIDTH - i, HEIGHT);
+        }
     }
 
     // Don't touch the code below
