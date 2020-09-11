750c95133f340dd737fcd800a26438466145e956
Zuzana <saska.stasova@gmail.com>
2020-09-11 14:19:12 +0200

    add EnvelopeStar

--- week-02/day5/src/com/company/EnvelopeStar.java
+++ week-02/day5/src/com/company/EnvelopeStar.java
@@ -5,35 +5,37 @@ import java.awt.*;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class EnvelopeStar {
-    public static void mainDraw(Graphics graphics) {
-        for (int numberOfLines = 0; numberOfLines < WIDTH/2; numberOfLines += 10) {
-            graphics.setColor(Color.GREEN);
-            graphics.drawLine(WIDTH/2, 0, WIDTH/2,HEIGHT/2);
+        public static void mainDraw(Graphics graphics) {
+            for (int i = 0; i < WIDTH/2 ; i+=10) {
+                graphics.setColor(Color.GREEN);
+                graphics.drawLine(WIDTH/2, 0 + i, WIDTH/2 - i, HEIGHT/2);
+                graphics.drawLine(0 + i, HEIGHT/2, WIDTH/2, HEIGHT/2 + i);
+                graphics.drawLine(WIDTH/2, HEIGHT - i, WIDTH/2 + i, HEIGHT/2);
+                graphics.drawLine(WIDTH - i, HEIGHT/2, WIDTH/2, HEIGHT/2 - i);
+            }
         }
 
-    }
+        // Don't touch the code below
+        static int WIDTH = 320;
+        static int HEIGHT = 320;
 
-    // Don't touch the code below
-    static int WIDTH = 320;
-    static int HEIGHT = 320;
-
-    public static void main(String[] args) {
-        JFrame jFrame = new JFrame("Drawing");
-        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        LinePlay.ImagePanel panel = new LinePlay.ImagePanel();
-        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        jFrame.add(panel);
-        jFrame.setLocationRelativeTo(null);
-        jFrame.setVisible(true);
-        jFrame.pack();
-    }
-
-    static class ImagePanel extends JPanel {
-        @Override
-        protected void paintComponent(Graphics graphics) {
-            super.paintComponent(graphics);
-            mainDraw(graphics);
+        public static void main(String[] args) {
+            JFrame jFrame = new JFrame("Drawing");
+            jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+            ImagePanel panel = new ImagePanel();
+            panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+            jFrame.add(panel);
+            jFrame.setLocationRelativeTo(null);
+            jFrame.setVisible(true);
+            jFrame.pack();
         }
-    }
+
+        static class ImagePanel extends JPanel {
+            @Override
+            protected void paintComponent(Graphics graphics) {
+                super.paintComponent(graphics);
+                mainDraw(graphics);
+            }
+        }
 
 }
