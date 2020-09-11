2b46537531d6407592361d5fbc9d09b24b33cdb6
Zuzana <saska.stasova@gmail.com>
2020-09-11 13:48:18 +0200

    add LinePlay

---
+++ week-02/day5/src/com/company/LinePlay.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.HashMap;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class LinePlay {
+    public static void mainDraw(Graphics graphics) {
+        for (int numberOfLines = 0; numberOfLines < WIDTH; numberOfLines += 20) {
+            graphics.setColor(Color.magenta);
+            graphics.drawLine(0 + numberOfLines,0, WIDTH, 20 + numberOfLines);
+            graphics.drawLine(WIDTH, HEIGHT - numberOfLines, WIDTH - numberOfLines - 20,0);
+        }
+        for (int numberOfLines = 0; numberOfLines < WIDTH; numberOfLines += 20) {
+            graphics.setColor(Color.GREEN);
+            graphics.drawLine(0 ,0 + numberOfLines, 20 + numberOfLines, HEIGHT);
+            graphics.drawLine(WIDTH - numberOfLines, HEIGHT , 0,HEIGHT - numberOfLines - 20);
+
+        }
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+
+}
