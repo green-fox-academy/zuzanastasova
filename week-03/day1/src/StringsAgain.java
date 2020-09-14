5ac36de6f57954c93897c85676998d30db4a1bfc
Zuzana <saska.stasova@gmail.com>
2020-09-14 16:37:11 +0200

    add StringAgain

--- week-03/day1/src/StringsAgain.java
+++ week-03/day1/src/StringsAgain.java
@@ -1,16 +1,15 @@
 public class StringsAgain {
     public static void main(String[] args) {
-        System.out.println("Hello, whx are xou here?");
+        System.out.println(strings("Hello, whx are xou here?"));
     }
 
     public static String strings (String string) {
         if (string.length() == 0)
             return string;
         else if (string.charAt(0) == 'x') {
-            string = string.substring(0,1) + string.substring(1);
+            string = string.substring(0, 0) + string.substring(1);
         }
-
-        return strings (string.substring(1));
+        return  string.charAt(0) + strings(string.substring(1)) ;
     }
 
     //Given a string, compute recursively a new string where all the 'x' chars have been removed.
