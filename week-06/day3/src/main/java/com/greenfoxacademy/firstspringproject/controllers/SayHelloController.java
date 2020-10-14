df27664af888a4a1fd320b0dd14e00e6d16bc57c
Zuzana <saska.stasova@gmail.com>
2020-10-14 16:32:13 +0200

    add SayHello

---
+++ week-06/day3/src/main/java/com/greenfoxacademy/firstspringproject/controllers/SayHelloController.java
@@ -0,0 +1,31 @@
+package com.greenfoxacademy.firstspringproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Random;
+
+@Controller
+public class SayHelloController {
+
+    String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+            "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+            "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+            "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
+    @RequestMapping(value = "/sayhello")
+    public String sayHello(Model model,
+                           @RequestParam(required = false, defaultValue = "blue") String color,
+                           @RequestParam(defaultValue = "30") int fontSize) {
+        Random random = new Random();
+        int index = random.nextInt(hellos.length);
+        String randomHello = hellos[index];
+        model.addAttribute("randomHello", randomHello);
+        model.addAttribute("color", color);
+        model.addAttribute("fontSize", fontSize);
+        return "sayhello";
+    }
+
+}
