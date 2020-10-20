5883ad9c0efafe0bc71c3b97fd4949044b29cf97
Zuzana <saska.stasova@gmail.com>
2020-10-20 09:58:33 +0200

    add MySQL Movies

--- week-07/day1/HP527_03(20200903 120239).sql
+++ week-07/day1/HP527_03(20200903 120239).sql
@@ -1,2 +1,79 @@
-select mid, title from Movie where director = "Steven Spielberg";
+SELECT 
+	mID, 
+	title 
+FROM 
+	movie 
+WHERE 
+	director = "Steven Spielberg";
 
+SELECT DISTINCT 
+	year 
+FROM 
+	movie 
+JOIN 
+	rating on movie.mID = rating.mID 
+WHERE 
+	rating.stars >= 4 
+ORDER BY 
+	movie.year asc;
+
+SELECT 
+	title 
+FROM 
+	movie 
+WHERE 
+	mID NOT IN (
+		SELECT 
+			mID 
+		FROM 
+			rating);
+
+SELECT DISTINCT 
+	name 
+FROM 
+	reviewer 
+JOIN 
+	rating on reviewer.rID = rating.rID 
+WHERE 
+	rating.ratingDate is NULL;
+
+SELECT 
+	reviewer.name, 
+	movie.title, 
+	rating.stars, 
+    rating.ratingdate 
+FROM 
+	movie 
+JOIN 
+	rating on movie.mID = rating.mID 
+JOIN 
+	reviewer on rating.rID = reviewer.rID 
+ORDER BY 
+	reviewer.name, 
+    movie.title, 
+    rating.stars;
+
+select
+    title,
+    name, 
+    (select stars from rating where rating.ratingDate = tempTable.firstRatingDate and rating.mID = tempTable.mID and rating.rID = tempTable.rID) as firstStars,
+    (select stars from rating where rating.ratingDate = tempTable.latestRatingDate and rating.mID = tempTable.mID and rating.rID = tempTable.rID) as lastStars
+from (
+    select title,
+        name,
+        movie.mID,
+        rating.rID,
+        max(ratingDate) as latestRatingDate,
+        min(ratingDate) as firstRatingDate,
+        count(ratingDate) as rateCount
+     from movie
+        left join rating on rating.mID = movie.mID
+        left join reviewer on rating.rID = reviewer.rID
+        group by title, name, mID, rID
+        having rateCount > 1
+        ) tempTable
+	having 
+    lastStars > firstStars;
+    
+
+    
