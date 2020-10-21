c727ed1ecba6856befabf28b50cc881507f0fbbe
Zuzana <saska.stasova@gmail.com>
2020-10-21 17:54:25 +0200

    add MessageService

--- week-07/day3/messageservice/src/main/java/com/greenfoxacademy/messageservice/configurations/MessageConfiguration.java
+++ week-07/day3/messageservice/src/main/java/com/greenfoxacademy/messageservice/configurations/MessageConfiguration.java
@@ -1,5 +1,17 @@
 package com.greenfoxacademy.messageservice.configurations;
 
+
+import com.greenfoxacademy.messageservice.services.MessageService;
+import com.greenfoxacademy.messageservice.services.TwitterService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MessageConfiguration {
+
+    @Bean
+    public MessageService messageService() {
+        return new TwitterService();
+    }
 
 }
--- week-07/day3/messageservice/src/main/java/com/greenfoxacademy/messageservice/services/TwitterService.java
+++ week-07/day3/messageservice/src/main/java/com/greenfoxacademy/messageservice/services/TwitterService.java
@@ -2,6 +2,7 @@ package com.greenfoxacademy.messageservice.services;
 
 import org.springframework.stereotype.Service;
 
+@Service
 public class TwitterService implements MessageService{
 
     @Override
