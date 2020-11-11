0d9d55451d266b0f01a68f8fad963cfc4ab484b7
Zuzana <saska.stasova@gmail.com>
2020-11-11 22:50:37 +0100

    add examtraining

--- week-10/trainingexam/src/main/java/com/greenfoxacademy/orientationexamtraining/repositories/UrlAliasRepository.java
+++ week-10/trainingexam/src/main/java/com/greenfoxacademy/orientationexamtraining/repositories/UrlAliasRepository.java
@@ -4,8 +4,6 @@ import com.greenfoxacademy.orientationexamtraining.models.UrlAlias;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface UrlAliasRepository extends JpaRepository<UrlAlias, Long> {
 
